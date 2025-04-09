e1ae5a97f6aa774e47c90cefae8675566d90b0b8
Pedro Borges <83559896+SunP04@users.noreply.github.com>
2025-04-09 22:09:50 +0000

    feat: login page (not centered)

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,27 @@
+import { twMerge } from "tailwind-merge";
+
+function InputField({ className, ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
+    return <input className={twMerge(className, "w-full text-md px-5 py-3 bg-[#D9D9D9] text-[#888888] rounded-0")} {...props} />
+}
+
+export default function LoginPage() {
+    const onLogin = async (form: FormData) => {
+        "use server";
+
+        // TODO: Implement login to backend
+        void form;
+    }
+
+    return (
+        <form 
+            action={onLogin}
+            className="flex flex-col w-full h-full max-w-md gap-y-5 items-center justify-center"
+        >
+            <InputField type="text" name="login" id="login" placeholder="Login" />
+            <InputField type="password" name="senha" id="senha" placeholder="Senha" />
+            <button className="bg-[#333333] text-white px-12 py-2">
+                Login
+            </button>
+        </form>
+    );
+}
