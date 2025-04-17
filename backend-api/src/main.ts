04cafea81754669f662992ab23adc8f8cfc49734
Enzo Suzart <suzart_@outlook.com>
2025-04-16 22:28:33 -0300

    feat: Adiciona log ao iniciar aplicacao

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+bootstrap()
+.then(() => {
+  console.log(`API iniciou com sucesso.`);
+})
+.catch((error) => {
+  console.error('Erro iniciando a API: ', error);
+} );
