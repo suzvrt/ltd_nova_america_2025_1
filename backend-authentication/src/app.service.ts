cf7ba5ca83947ca2f1b829827ae30eb6ff63a934
Enzo Suzart <suzart_@outlook.com>
2025-04-16 22:42:28 -0300

    feat: Adiciona conexao ao banco de dados ao auth

--- backend-authentication/src/app.service.ts
+++ backend-authentication/src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from './users/users.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly userService: UsersService) {}
+
   getHello(): string {
-    return 'Hello World! Backend-authentication - atualzando em tempo real';
+    return 'Hello World! backend-authentication';
   }
 }
